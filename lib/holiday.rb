423ce207eab068e1e79b3b8cc0bf5ca67e0427f7
virtual-frame-7789 <mttwright@gmail.com>
2019-09-22 23:21:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -102,7 +102,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.each do |season, holidays|
       holidays.each do |holiday, supplies|
         if supplies.include?("BBQ")
-          bbq_array.pushholiday
+          bbq_array.push(holiday)
         end
       end
     end
