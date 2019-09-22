aa68a07420bd25bd2769f92927d81822a3747792
virtual-frame-7789 <mttwright@gmail.com>
2019-09-22 19:23:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 
   holiday_hash.each do |season, holiday|
     if season == :winter
-      binding.pry
+      holiday.each do |holiday, stuff|
+        binding.pry
+      end
     end
   end
 
