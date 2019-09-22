5043715b799343fecefb8785a126ebd3052fbfc9
virtual-frame-7789 <mttwright@gmail.com>
2019-09-22 23:08:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |holiday, supplies|
-      str_holiday = holiday.to_s.split("_").collect {|word| word.capitalize!
+      str_holiday = holiday.to_s.split("_").collect {|word| word.capitalize!}
     end
   end
   
