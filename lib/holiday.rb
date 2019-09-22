505beb8a29a6f667b837ca73a104f6e2efa3d9fe
virtual-frame-7789 <mttwright@gmail.com>
2019-09-22 19:48:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   collection = []
   holiday_hash[:winter].each do |holiday, stuff|
-    holiday.collect do |supplies|
+    holiday.each do |supplies|
       collection.push(supplies)
       binding.pry
     end
