0bf0df9cbb4f5601776b7c2235aca7e62106df09
virtual-frame-7789 <mttwright@gmail.com>
2019-09-22 22:08:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,6 +85,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |holiday, supplies|
+      word_array = []
+      holiday
       binding.pry
     end
   end
